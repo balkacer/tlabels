589dc8594917a6096363eb66d187532baf4b6b1b
enmanuel.balcacer <enmanuel.balcacer@atl-software.net>
2021-08-13 14:25:13 -0400

    Adding files to be ignored by npm and git

--- src/__tests__/labels.test.ts
+++ src/__tests__/labels.test.ts
@@ -1,18 +1,51 @@
 import { tlbs } from '../index';
-const lbs = tlbs('es', ['es', 'en'], '/Users/developer/Downloads/py/labels/');
+const lbs = tlbs('es', ['es', 'en'], '/Users/developer/Downloads/py/labels/json');
 
-test('ceate a label', async () => {
-  await lbs.insertOrEdit([
-    {
-      name: 'label_1',
-      values: {
-        es: 'verdadero',
-        en: 'true',
-      },
-    },
-  ]);
+test('ceate and get a label', async () => {
+    await lbs.insertOrEdit([
+        {
+            name: 'label_1',
+            values: {
+                es: 'verdadero',
+                en: 'true',
+            },
+        },
+    ]);
 
-  return lbs.get('label_1').then((data) => {
-    expect(data).toBe('verdadero');
-  });
+    return lbs.get('label_1').then((data) => {
+        expect(data).toBe('verdadero');
+    });
+});
+
+test('ceate and edit a label', async () => {
+    let first = null;
+    let second = null;
+
+    await lbs.insertOrEdit([
+        {
+            name: 'label_2',
+            values: {
+                es: '¡todo estu bien!',
+                en: 'all right!',
+            },
+        },
+    ]);    
+    await lbs.get('label_2').then((data) => {
+        first = data;
+    });
+    await lbs.insertOrEdit([
+        {
+            name: 'label_2',
+            values: {
+                es: '¡nada estu bien!',
+                en: 'nothing be right!',
+            },
+        },
+    ]);
+    await lbs.get('label_2').then((data) => {
+        second = data
+    });
+
+    expect(first).toBe('¡todo estu bien!');
+    expect(second).toBe('¡nada estu bien!');
 });
