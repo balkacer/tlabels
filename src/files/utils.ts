24b1a146bcbc3860d0e1e68d4846b53cbb211429
balcacer1 <112181410+balcacer1@users.noreply.github.com>
2022-10-04 15:53:16 -0400

    something get

--- src/files/utils.ts
+++ src/files/utils.ts
@@ -1,3 +1,6 @@
 export function toSnakeCase(text: string): string {
-  return text.replace(/([A-Z])/g, (match) => `_${match.toLowerCase()}`).replace(/^_/, '');
+  return text.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`).replace(/\W+/g, " ")
+    .split(/ |\B(?=[A-Z])/)
+    .map(word => word.toLowerCase())
+    .join('_');
 }
